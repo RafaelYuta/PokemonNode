9d1088571b5f7a7c9913ba002b4016b35923c80a
RafaelYuta <126525139+RafaelYuta@users.noreply.github.com>
2024-11-01 20:05:43 -0300

    Delete Pasta/Pasta directory

--- Pasta/Pasta/src/model/pokemon.js
+++
@@ -1,9 +0,0 @@
-class pokemon {
-    constructor(nome, tipo, altura, peso, nivelDePoder) {
-        this.nome = nome;
-        this.tipo = tipo;
-        this.altura = altura;
-        this.peso = peso;
-        this.nivelDePoder = nivelDePoder;
-    }
-}
